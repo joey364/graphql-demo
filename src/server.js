a03016e5c439d462515fbdbfd9aeebcd592cfcef
joey364 <71462893+joey364@users.noreply.github.com>
2021-12-18 02:25:52 +0000

    use database on localhost

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
-const express = require("express");
-const app = express();
-const { graphqlHTTP } = require("express-graphql");
-const PORT = 3000;
-const schema = require("./schema");
+const express = require("express")
+const mongoose = require('mongoose')
+const { graphqlHTTP } = require("express-graphql")
 
+const app = express()
+const schema = require("./schema")
+
+const PORT = process.env.PORT ?? 3000;
+
+mongoose.connect('http://localhost:27017/graphql-demo', (err) => {
+  if (err) {
+    console.warn('Not connected to database')
+    console.error(err)
+  }
+  else { console.log('Connected to database') }
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// main entry point 
 app.use(
   "/graphql",
   graphqlHTTP({
